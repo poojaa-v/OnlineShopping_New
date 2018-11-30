d6c7497c95ee77d8f078f053df325fc3bfbb60b3
poojaa-v <pooja.venkataraman@cognizant.com>
2018-11-30 18:09:09 +0530

    Base version

--- customer_117694/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-insert into customer_117694(id,email,first_name,last_name) values('1','p@gmail.com','Poo','Ven');
-insert into customer_117694(id,email,first_name,last_name) values('2','m@gmail.com','Mon','Ra');
-
-
--- customer_117694/src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS customer_117694;
-
-CREATE TABLE customer_117694
-(
-    id varchar(36) NOT NULL,
-    email varchar(200) DEFAULT NULL,
-    first_name varchar(500) DEFAULT NULL,
-    last_name varchar(500) DEFAULT NULL   
-);
