5866b4bd6ab6922577925c15149b590379727084
poojaa-v <pooja.venkataraman@cognizant.com>
2018-11-30 18:02:39 +0530

    Config Server - Base version

---
+++ Config_117694/src/main/java/io/online/salesorder/Config117694Application.java
@@ -0,0 +1,14 @@
+package io.online.salesorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Config117694Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Config117694Application.class, args);
+	}
+}
