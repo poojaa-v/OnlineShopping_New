f2b24bd50771ab0907ad03273e7f42eb7517bb0d
poojaa-v <pooja.venkataraman@cognizant.com>
2018-11-30 18:07:55 +0530

    Base version

---
+++ SalesOrderService_117694/src/main/java/io/online/salesorder/SalesOrderService117694Application.java
@@ -0,0 +1,32 @@
+package io.online.salesorder;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
+import io.online.salesorder.controller.SalesOrderController;
+import io.online.salesorder.domain.Customer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableHystrix
+public class SalesOrderService117694Application {
+
+	@Autowired
+	private SalesOrderController salesOrderController;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SalesOrderService117694Application.class, args);
+	}
+	
+	@RabbitListener(queues = "CustomerCreated")
+	public void receiveMessage(Customer customer) {
+		System.out.println("Customer Details" + customer.getId());
+		salesOrderController.insertCustomerSOS(customer);
+		
+	}
+	
+}
